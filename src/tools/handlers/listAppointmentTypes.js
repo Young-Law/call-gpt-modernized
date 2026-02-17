f8f71dadd9e2ff7a41d3f14f0106fbd8b23d8784
Daniel Wycoff <daniel.w@eorumyoung.com>
2026-02-17 06:57:26 -0600

    should be green-light'd

--- src/tools/handlers/listAppointmentTypes.js
+++
@@ -1,7 +0,0 @@
-const { listAppointmentTypes } = require('../../integrations/zoho/client');
-
-async function listAppointmentTypesHandler() {
-  return { status: 'success', appointment_types: listAppointmentTypes() };
-}
-
-module.exports = { listAppointmentTypesHandler };
--- src/tools/handlers/listStaffMembers.js
+++
@@ -1,7 +0,0 @@
-const { listStaffMembers } = require('../../integrations/zoho/client');
-
-async function listStaffMembersHandler() {
-  return { status: 'success', staff_members: listStaffMembers() };
-}
-
-module.exports = { listStaffMembersHandler };
